e2895ab780371c30f7c4a2cea4e48533a07e5de5
Alex Angel <alex@Alexs-MacBook-Pro.local>
2020-08-02 15:50:53 -0400

    complete game

--- game.js
+++ game.js
@@ -1,14 +1,60 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var gameStarted = false;
 
+
+// Detect when the buttons are pressed
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  animatePress(userChosenColour);
   playSound(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
 });
 
+// Detect keypress to begin the game
+$(document).keypress(function(event) {
+  if (!gameStarted) {
+    $("h1#level-title").text("Level " + level);
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) { // success
+      //move on to next Level
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else { // if the user gets the sequence wrong
+    var wrong = new Audio("sounds/wrong.mp3");
+    wrong.play();
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+//if the user loses
+function startOver() {
+  level = 0;
+  gameStarted = false;
+  gamePattern = [];
+}
+
+// Main game loop
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -16,7 +62,17 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
+// Plays sounds of button
 function playSound(name) {
   var sound = new Audio("sounds/" + name + ".mp3");
   sound.play();
+}
+
+// Animates button when pressed
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
